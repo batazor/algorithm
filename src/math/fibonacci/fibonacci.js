b5b6ebe832758ce41d19dd7c1e2fbd9c2bcdd116
Victor Login <batazor111@gmail.com>
2019-01-27 02:53:02 +0300

    js: fibonacci (Iterator and Iterable protocol)

--- src/math/fibonacci/fibonacci.js
+++ src/math/fibonacci/fibonacci.js
@@ -8,7 +8,7 @@
  * fib() // 5
  * fib() // null
  */
-export const genFib = (max = Number.MAX_SAFE_INTEGER) => {
+export const genFibFunc = (max = Number.MAX_SAFE_INTEGER) => {
   // initialize default values in the scope
   let n1 = 0
   let n2 = 0
@@ -30,5 +30,52 @@ export const genFib = (max = Number.MAX_SAFE_INTEGER) => {
     }
 
     return nextVal
+  }
+}
+
+/**
+ * @example
+ * const fib = genFib(6) // { next: [Function: next] }
+ * fib.next() // { value: 1, done: false }
+ * fib.next() // { value: 1, done: false }
+ * fib.next() // { value: 2, done: false }
+ * fib.next() // { value: 3, done: false }
+ * fib.next() // { value: 5, done: false }
+ * fib.next() // { value: undefined, done: true }
+ */
+export const genFibIterator = (max = Number.MAX_SAFE_INTEGER) => {
+  // initialize default values in the scope
+  let n1 = 0
+  let n2 = 0
+
+  // this time we return an iterator object (rather than a function)
+  return {
+    // this satisfies the Iterator protocol
+    next: () => {
+      // calculates the next value
+      let nextVal = n2 === 0 ? 1 : n1 + n2
+
+      // redefines n1 and n2 to match new values
+      const prevVal = n2
+      n2 = nextVal
+      n1 = prevVal
+
+      // if we reached the upper bound (iteration completed)
+      // set done to true and nextVal to undefined
+      let done = false
+      if (nextVal >= max) {
+        nextVal = undefined
+        done = true
+      }
+
+      // return the iteration object as for the iteration protocol
+      return { value: nextVal, done }
+    },
+
+    // this satisfies the Iterable protocol
+    [Symbol.iterator] () {
+      // returns `this` because the object itself is an iterator
+      return this
+    }
   }
 }
