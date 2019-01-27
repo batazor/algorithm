8ceb20dac83adf0887c15883fe9ff5ace9965869
Victor Login <batazor111@gmail.com>
2019-01-27 03:50:16 +0300

    js: fibonacci (generator)

--- src/math/fibonacci/fibonacci.js
+++ src/math/fibonacci/fibonacci.js
@@ -42,6 +42,9 @@ export const genFibFunc = (max = Number.MAX_SAFE_INTEGER) => {
  * fib.next() // { value: 3, done: false }
  * fib.next() // { value: 5, done: false }
  * fib.next() // { value: undefined, done: true }
+ * @example
+ * const fib = genFib(6) // { next: [Function: next] }
+ * [...fib] // [1, 1, 2, 3, 5]
  */
 export const genFibIterator = (max = Number.MAX_SAFE_INTEGER) => {
   // initialize default values in the scope
@@ -77,5 +80,28 @@ export const genFibIterator = (max = Number.MAX_SAFE_INTEGER) => {
       // returns `this` because the object itself is an iterator
       return this
     }
+  }
+}
+
+export function* genFib (max = Number.MAX_SAFE_INTEGER) {
+  // initialize default values in the scope
+  let n1 = 0
+  let n2 = 0
+
+  // we can now pre-initialize nextVal to 1 as part of the this.state
+  let nextVal = 1
+
+  // loop until we exceed the max number
+  while (nextVal <= max) {
+    // yields the current value
+    yield nextVal
+
+    // shifts nextVal -> v2 and n2 -> n1
+    const prevVal = n2
+    n2 = nextVal
+    n1 = prevVal
+
+    // calculates the next value
+    nextVal = n1 + n2
   }
 }
