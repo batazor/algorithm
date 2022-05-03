6cc97077f21ca1f3f31790a460c6bd53da478353
Victor Login <batazor111@gmail.com>
2022-05-03 22:16:33 +0300

    queue: add queue (golang with generics)

--- src/struct/thunk/thunk.go
+++ src/struct/thunk/thunk.go
@@ -4,6 +4,11 @@ import (
 	"github.com/batazor/algorithm/src/struct/thunk/options"
 )
 
+type Thunk[T any] struct {
+	doer func() T           // action being thunked
+	o    *options.Option[T] // cache for complete thunk data
+}
+
 func New[T any](doer func() T) *Thunk[T] {
 	return &Thunk[T]{
 		doer: doer,
