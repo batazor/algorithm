af5d937508404bcd30906607b787ff91d7321a40
Victor Login <batazor111@gmail.com>
2022-05-03 21:52:51 +0300

    thunk: add golang implementation with use generics (#13)

---
+++ src/struct/thunk/thunk_test.go
@@ -0,0 +1,48 @@
+package thunk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestThunkFib(t *testing.T) {
+	cache := make([]*Thunk[int], 41)
+
+	var fib func(int) int
+	fib = func(n int) int {
+		if cache[n].o.IsSome() {
+			return cache[n].o.Apply()
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	for i := range cache {
+		i := i
+		cache[i] = New(func() int { return fib(i) })
+	}
+	cache[0].o.Set(0)
+	cache[1].o.Set(1)
+
+	assert.Equal(t, cache[40].Force(), 102334155)
+}
+
+func TestMemoizedFib(t *testing.T) {
+	mem := map[int]int{
+		0: 0,
+		1: 1,
+	}
+
+	var fib func(int) int
+	fib = func(n int) int {
+		if result, ok := mem[n]; ok {
+			return result
+		}
+
+		result := fib(n-1) + fib(n-2)
+		mem[n] = result
+		return result
+	}
+
+	assert.Equal(t, fib(40), 102334155)
+}
