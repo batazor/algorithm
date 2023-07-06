bb9e25576c6c290b6ab4fb5b62108bda69d9f688
Victor Login <batazor@evrone.com>
2023-07-06 03:23:36 +0200

    sort: add merge sort
    Signed-off-by: Victor Login <batazor@evrone.com>

--- src/sorting/merge/merge.go
+++ src/sorting/merge/merge.go
@@ -1,6 +1,8 @@
 package merge
 
 import (
+	sort2 "sort"
+
 	merge_two_sort_array "github.com/batazor/algorithm/src/leetCode/88"
 )
 
@@ -9,6 +11,8 @@ func Sort(arr []int) []int {
 }
 
 func merge(arr []int) []int {
+	sort2.Sort(sort2.IntSlice(arr))
+
 	if len(arr) > 2 {
 		a := arr[:(len(arr)-1)/2]
 		b := arr[(len(arr)-1)/2:]
